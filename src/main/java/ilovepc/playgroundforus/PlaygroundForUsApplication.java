524aec92cc03867eaec5789fabe59fcb6aa7b540
정승주 <ilovesecu@gmail.com>
2023-03-24 17:40:09 +0900

    Initial commit

---
+++ src/main/java/ilovepc/playgroundforus/PlaygroundForUsApplication.java
@@ -0,0 +1,13 @@
+package ilovepc.playgroundforus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundForUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundForUsApplication.class, args);
+    }
+
+}
