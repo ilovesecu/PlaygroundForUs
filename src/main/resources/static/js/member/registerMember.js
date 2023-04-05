e7ed0b0e6e57ab351538b1c06420b233aab38d10
정승주 <ilovesecu@gmail.com>
2023-04-05 13:51:53 +0900

    -회원가입 페이지 다듬기 [-ing] -회원가입 프론트 예외처리[-ing] -회원가입 ID, Password 실시간 체크[-will] -회원가입 처리[-will]

--- src/main/resources/static/js/member/registerMember.js
+++ src/main/resources/static/js/member/registerMember.js
@@ -22,16 +22,38 @@ class RegisterMember{
             const val = e.target.value;
             const response = await axios.get(`/member/rest/duple/${val}`);
             const result = response.data;
-            if(result.data === 1){ //중복체크 OK
 
-            }else{ //중복체크 NO
-                this.inputValid(e.target, 'is-invalid');
+            let $idInputValid = this.doms.$idInputValid ?? null;
+            if(!$idInputValid){
+                $idInputValid = this.doms.$idInput.parentElement.querySelector("div.valid");
+                this.doms.$idInputValid = $idInputValid;
             }
+            const param = {
+                $dom : e.target,
+                $validDom : $idInputValid,
+            }
+
+            if(result.data === 1){ //중복체크 OK
+                param.domClassName='is-valid',
+                param.validClassName = 'valid-text';
+                param.validText = '사용가능한 아이디입니다.';
+            }else{ //중복체크 NO
+                param.domClassName='is-invalid',
+                param.validClassName = 'invalid-text';
+                param.validText = '이미 존재하는 아이디입니다.';
+            }
+            this.inputValid(param);
         });
     }
 
-    inputValid($dom, className='is-invalid', subTxt=""){
-        $dom.classList.add(className);
+    inputValid({$dom, $validDom, domClassName, validClassName, validText}){
+        $dom.classList.remove('is-valid','is-invalid');
+        $dom.classList.add(domClassName);
+        if($validDom){
+            $validDom.classList.remove('valid-text','invalid-text');
+            $validDom.classList.add(validClassName);
+            $validDom.textContent = validText;
+        }
     }
 
 }
