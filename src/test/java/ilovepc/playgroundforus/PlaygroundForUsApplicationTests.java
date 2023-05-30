85cc0c136a9b2f833f42f456a647d4e5616697a9
정승주 <ilovesecu@gmail.com>
2023-05-30 16:44:32 +0900

    no commit

---
+++ src/test/java/ilovepc/playgroundforus/PlaygroundForUsApplicationTests.java
@@ -0,0 +1,13 @@
+package ilovepc.playgroundforus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundForUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
